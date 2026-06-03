2cb1824cb66ae6a08648c58b3699ce0b86094728
Emir Kara <121714055+emirgit@users.noreply.github.com>
2026-06-03 21:16:15 +0300

    Update capacitor.config.ts

--- mobile/capacitor.config.ts
+++ mobile/capacitor.config.ts
@@ -3,7 +3,11 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.group11.turnstile',
   appName: 'Turnstile Display',
-  webDir: 'dist'
+  webDir: 'dist',
+  server: {
+    androidScheme: 'http',
+    cleartext: true
+  }
 };
 
 export default config;
